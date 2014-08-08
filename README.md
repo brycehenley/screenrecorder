3a512b9319eacbfc1af5ab6d1bb9e1a803fda584
brycehenley <brycehenley97@gmail.com>
2014-08-08 00:09:00 -0700

    added comments and added to README

--- README.md
+++ README.md
@@ -4,5 +4,8 @@ screenrecorder
 high performance screen recording bash scrip for linux 
 tested on kubuntu 14.04
 
+This scrip uses libav to record x11 output in lossless HD quailty using h.264 multithreaded encoding 
+(this does not include audio)
+
 Dependencies:
 libav
--- screenrecord.sh
+++ screenrecord.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
+#
+# this scrip uses libav to record x11 output in lossless HD quailty using h.264 multithreaded encoding
+
 avconv -f x11grab -r 30 -s 1920x1080 -i :0.0+0,0 -vcodec libx264 -pre lossless_ultrafast -threads auto VideoOutput.mp4
+
+# -r flag sets frame rate defualt = 30
+# -s flag sets resolution
+# -i flag sets screen postion
+# videocodec is h.264 lossless with .mp4 file output
+# -threads is set to automaticaly assign threads to your total number of cpu cores 
